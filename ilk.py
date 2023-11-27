266ed2cccde70671a1ef6ad865867c50a4950237
betaegitim <info@betaegitim.com>
2023-11-27 07:15:51 +0000

    27112023_1

--- ilk.py
+++ ilk.py
@@ -1,1 +1,12 @@
-
+# import os
+# import shutil
+# liste = ["HakanOzden","HakanOzel","GamzeCelik","FurkanKeles","MerveSarpBoyar","OzgeCillik","SimgeAydin","IpekDolu"]
+# fileName = "ilk.ipynb"
+# for item in liste:
+#     if not os.path.exists(f"Egzersizler/{item}"):
+#         os.mkdir(f"Egzersizler/{item}")
+#     # open(f"Egzersizler/{item}/{fileName}","a+")
+#     kaynakadres = f"Egzersizler/SoruCevaplar/{fileName}" 
+#     hedefadres = f"Egzersizler/{item}/{fileName}"
+#     shutil.copy(kaynakadres,hedefadres)
+print("Merhaba")
