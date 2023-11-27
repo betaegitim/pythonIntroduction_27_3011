bde6fdee5109e9b12b89b070ef2a6cd99a9bb600
betaegitim <info@betaegitim.com>
2023-11-27 08:17:12 +0000

    27112023_5

--- ilk.py
+++ ilk.py
@@ -1,12 +1,12 @@
-# import os
-# import shutil
-# liste = ["HakanOzden","HakanOzel","GamzeCelik","FurkanKeles","MerveSarpBoyar","OzgeCillik","SimgeAydin","IpekDolu"]
-# fileName = "ilk.ipynb"
-# for item in liste:
-#     if not os.path.exists(f"Egzersizler/{item}"):
-#         os.mkdir(f"Egzersizler/{item}")
-#     # open(f"Egzersizler/{item}/{fileName}","a+")
-#     kaynakadres = f"Egzersizler/SoruCevaplar/{fileName}" 
-#     hedefadres = f"Egzersizler/{item}/{fileName}"
-#     shutil.copy(kaynakadres,hedefadres)
-print("Merhaba")
+import os
+import shutil
+liste = ["HakanOzden","HakanOzel","GamzeCelik","FurkanKeles","MerveSarpBoyar","OzgeCillik","SimgeAydin","IpekDolu"]
+fileName = "01_02_Degiskenler.ipynb"
+for item in liste:
+    if not os.path.exists(f"Egzersizler/{item}"):
+        os.mkdir(f"Egzersizler/{item}")
+    # open(f"Egzersizler/{item}/{fileName}","a+")
+    kaynakadres = f"Egzersizler/SoruCevaplar/{fileName}" 
+    hedefadres = f"Egzersizler/{item}/{fileName}"
+    shutil.copy(kaynakadres,hedefadres)
+# print("Merhaba")
